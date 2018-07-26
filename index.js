721764db5d9e5c7e0e3f670e6497d609065fd3ba
QMaximillian <quinnlashinsky@gmail.com>
2018-07-26 14:04:37 -0400

    Added form element/ability to fetch from NYT api

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+document.addEventListener("DOMContentLoaded", init())
+
+
+	function init(){
+		document.getElementById('query').value
+		document.getElementById('begin-date').value
+		document.getElementById('end_date').value
+		document.getElementById('sort').value
+
+
+
+
+
+		// document.addEventListener('click', init(){
+			// 	function getFetchQuery() {
+			// 		fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=99fd1413c9ff4d13ab71707a05db43e0&q=dogs", {
+			// 			method: "GET",
+			// 		}).then(resp => resp.json()).then(articles => {
+			// 			console.log(articles)
+			// 		})
+			// 	}
+			// })
+
+
+	}
